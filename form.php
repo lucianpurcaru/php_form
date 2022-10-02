da7d1a05fb6407d3bd0a60b2901eac6cf47c98b0
Lucian Purcaru <97845722+lucianpurcaru@users.noreply.github.com>
2022-10-02 14:03:59 +0200

    form.php and form_css added

---
+++ form.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire</title>
+    <link rel="stylesheet" href="form_css.css">
+</head>
+<body>
+
+<form  action="/form.php"  method="post">
+    <div>
+      <label  for="nom">Nom :</label>
+      <input  type="text"  id="nom"  name="user_name">
+    </div>
+    <div>
+      <label  for="courriel">Courriel :</label>
+        <input  type="email"  id="courriel"  name="user_email">
+    </div>
+    <div>
+        <label for="phonenumber">Téléphone :</label>
+        <input type="tel" id="phonenumber" name="user_phone">
+    </div>
+    <div>
+        <label for="subject">Objet de la demande:</label>
+        <SELECT name="subject" id="subject" size="1">
+            <OPTION value="">Objet de ma demande</OPTION>
+            <OPTION>Souscription</OPTION>
+            <OPTION>Renseignements</OPTION>
+            <OPTION>Facture</OPTION>
+            <OPTION>Assistance</OPTION>
+            <OPTION>Autres informations</OPTION>
+        </SELECT>
+    </div>
+    <div>
+      <label  for="message">Message :</label>
+      <textarea  id="message"  name="user_message"></textarea>
+    </div>
+    <div  class="button">
+      <button  type="submit">Envoyer votre message</button>
+    </div>
+  </form>
+
+  <?php
+  var_dump($_POST);
+  ?>
+  <?php
+  echo $_POST['user_name'];
+  ?>
+
+</body>
+</html>
