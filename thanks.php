1c03e63f34d4a73c5ebbb6d0ce5ce9360076c690
lucianpurcaru <lucian.purcaru@gmail.com>
2022-10-03 08:06:47 +0200

    update

--- thanks.php
+++ thanks.php
@@ -11,8 +11,8 @@
 <p id="message">
 Merci <?php echo $_POST['user_firstname']; ?> <?php echo $_POST['user_lastname']; ?> de nous avoir contacté à propos de “<?php echo $_POST['subject']; ?>”.<br>
 
-Un de nos conseiller vous contactera soit à l’adresse <?php echo $_POST['user_mail']; ?> ou par téléphone au <?php echo $_POST['user_phone']; ?>} dans les plus brefs délais pour traiter votre demande : <br>
-<?php echo $_POST['message']; ?>
+Un de nos conseillers vous contactera soit à l’adresse <?php echo $_POST['user_email']; ?> ou par téléphone au <?php echo $_POST['user_phone']; ?> dans les plus brefs délais pour traiter votre demande : <br>
+<?php echo $_POST['user_message']; ?>
 
 </p>
 </body>
